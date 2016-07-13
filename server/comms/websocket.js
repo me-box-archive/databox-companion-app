3f63951c1128339cc2f34b8a954d70a61db02393
tlodge <tlodge@gmail.com>
2016-07-13 14:41:35 +0100

    adding in code to leave socketio rooms

--- server/comms/websocket.js
+++ server/comms/websocket.js
@@ -17,14 +17,20 @@ export default function init(nsps, server){
 
         nsp.on('connection', function(socket){
 
-          socket.on('join', function(app){
+           socket.on('join', function(app){
+              console.log("joining client to room" + app);
               socket.join(app);
-              return app; 
+              //return app; 
           });
 
+          socket.on('leave', function(app){
+            console.log("leaving room: " + app);
+            socket.leave(app);
+          });
+      
           socket.on('disconnect', function(){
-
-
+             console.log("socket disconnect!");
+             socket.leave(app);
           });
 
         });
@@ -34,6 +40,7 @@ export default function init(nsps, server){
 
 export function sendmessages(rooms, namespace, event, message){
   rooms.forEach(function(room){
+      console.log(`sending to room ${room} namespace ${namespace}`);
       this.sendmessage(room,namespace,event,message);
   }.bind(this));
   return rooms.length;
